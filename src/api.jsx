87dd6ffd362b29d5a3923db4fb6732805d264716
vittorioavataneo <vittorioavataneo@gmail.com>
2023-04-27 16:26:18 +0200

    aggiunge la registrazione paziente

---
+++ src/api.jsx
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+export const authenticate = async (email, password) => {
+    const response = await axios.post('http://localhost:8080/api/auth/authenticate',
+    {
+        "email": `${email}`,
+        "password": `${password}`,
+    });
+    localStorage.setItem("token",response.data.access_token);
+}
+
+export const registerPatient = async (firstname, lastname, dob, cellNumber, sex, email, password, taxCode) => {
+    const response = await axios.post('http://localhost:8080/api/auth/registration/patient',
+    {
+        "firstname" : `${firstname}`,
+        "lastname" : `${lastname}`,
+        "dob" : `${dob}`,
+        "cellNumber" : `${cellNumber}`,
+        "sex" : `${sex}`,
+        "email": `${email}`,
+        "password": `${password}`,
+        "taxCode": `${taxCode}`
+    });
+    localStorage.setItem("token_code",response.data.access_token);
+};
+
+export const registerDoctor = async (firstname, lastname, dob, cellNumber, sex, email, password, doctorCode, specialization, street, cap, city, province, country) => {
+    const response = await axios.post('http://localhost:8080/api/auth/registration/doctor',
+    {
+        "firstname" : `${firstname}`,
+        "lastname" : `${lastname}`,
+        "dob" : `${dob}`,
+        "cellNumber" : `${cellNumber}`,
+        "sex" : `${sex}`,
+        "email": `${email}`,
+        "password": `${password}`,
+        "doctorCode": `${doctorCode}`,
+        "address": {
+            "street": `${street}`,
+            "cap": `${cap}`,
+            "city": `${city}`,
+            "province": `${province}`,
+            "country": `${country}`,
+            
+        },
+        "specialization": `${specialization}`
+    });
+    localStorage.setItem("token",response.data.access_token);
+};
--- src/routes/patientRegistration.jsx
+++ src/routes/patientRegistration.jsx
@@ -1,43 +1,96 @@
 import { Form } from "react-router-dom";
+import { useState } from "react";
+import { registerPatient } from "../api";
+
 export default function PatientRegistration() {
+
+        const[firstname, setFirstname] = useState("");
+        const[lastname, setLastname] = useState("");
+        const[dob, setDob] = useState("");
+        const[email, setEmail] = useState("");
+        const[password, setPassword] = useState("");
+        const[taxCode, setTaxCode] = useState("");
+        const[cellNumber, setCellNumber] = useState("");
+        const[sex, setSex] = useState("");
+    
+        function handleSubmit(){
+            registerPatient(firstname, lastname, dob, cellNumber, sex, email, password, taxCode);
+        }
+    
+        function changeFirstname(e){
+            setFirstname(e.target.value);
+        }
+    
+        function changeLastname(e){
+            setLastname(e.target.value);
+        }
+    
+        function changeDob(e){
+            setDob(e.target.value);
+        }
+    
+        function changeEmail(e){
+            setEmail(e.target.value);
+        }
+    
+        function changePassword(e){
+            setPassword(e.target.value);
+        }
+
+        function changeTaxCode(e){
+            setTaxCode(e.target.value);
+        }
+
+        function changeCellNumber(e){
+            setCellNumber(e.target.value);
+        }
+
+        function changeSex(e){
+            setSex(e.target.value);
+        }
+
     return (
         <>
             <div id="registrazione">
                 <h2>Registrazione Paziente</h2>
-                <Form>
+                <Form onSubmit={handleSubmit}>
                     <label>
                         Nome:
-                        <input type="text" name="nome" />
+                        <input type="text" value={firstname} onChange={changeFirstname} required/>
                     </label>
                     <label>
                         Cognome:
-                        <input type="text" name="cognome" />
-                    </label>
-                    <label>
-                        Codice Fiscale:
-                        <input type="text" name="codiceFiscale" />
-                    </label>
-                    <label>
-                        Email:
-                        <input type="text" name="email" />
-                    </label>
-                    <label>
-                        Password:
-                        <input type="text" name="password" />
+                        <input type="text" value={lastname} onChange={changeLastname} required/>
                     </label>
                     <label>
                         Data di Nascita:
-                        <input type="text" name="dataNascita" />
-                    </label>
-                    <label>
-                        Sesso:
-                        <input type="text" name="sesso" />
+                        <input type="date" value={dob} onChange={changeDob} required/>
                     </label>
                     <label>
                         Numero di Telefono:
-                        <input type="text" name="numeroTelefono" />
+                        <input type="text" value={cellNumber} onChange={changeCellNumber} minLength={10} maxLength={10}/>
                     </label>
-                    <input type="submit" value="Registrati" />
+                    <label>
+                        Sesso:
+                        <select value={sex} onChange={changeSex} required>
+                            <option value="UOMO">Uomo</option>
+                            <option value="DONNA">Donna</option>
+                            <option value="NON_BINARIO">Non Binario</option>
+                        </select>
+                    </label>
+                    <label>
+                        Email:
+                        <input type="email" value={email} onChange={changeEmail} required/>
+                    </label>
+                    <label>
+                        Password:
+                        <input type="password" value={password} onChange={changePassword} required/>
+                    </label>
+                    <label>
+                        Codice Fiscale:
+                        <input type="text" value={taxCode} onChange={changeTaxCode} minLength={16} maxLength={16} required/>
+                    </label>
+                    <button type="submit">Registrati</button>
                 </Form>
             </div>
         </>
